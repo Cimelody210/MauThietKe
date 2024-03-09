595cdd2e94a937c22e125d8d35eed65fa7c47b63
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-03-09 21:05:52 +0700

    edit Dart

---
+++ Code dạo/demo.dart
@@ -0,0 +1,20 @@
+import 'package:decider/views.home_view.dart';
+import 'package:flutter/views.material.dart';
+
+
+void main(){
+    runApp(const MainApp());
+}
+
+class MainApp extends StateWidget{
+    const MainApp({super.key});
+}
+
+@override
+Widget build(BuildContext context){
+    return MaterialApp(
+        title: 'Decider',
+        theme: ThemDatea(primarySwatch: Colours.red),
+        home: const HomeView()
+    )
+}
