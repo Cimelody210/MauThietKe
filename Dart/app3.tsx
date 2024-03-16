d95ce43991b3c2d92cde69a309c189b608cbde9e
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-03-16 15:34:03 +0700

    Edit

--- Dart/app3.tsx
+++ Dart/app3.tsx
@@ -4,7 +4,12 @@ import {Alert, Button, Space} from 'antd';
 import {getIcon} from ".../../ultis/getAlertIcon";
 import {sendAnalytics, entry} from './until';
 import  './Alerts.scss';
-type AlertColor = "success" |"error"| "warning"| "info" | "grey";
+
+@Injectable({
+    proviedIn: 'root'
+})
+
+  type AlertColor = "success" |"error"| "warning"| "info" | "grey";
 
 function IDtable(){
     const [id, state] = userState(
@@ -28,9 +33,11 @@ const SharedAler: React.FC<SharedAlertProps>=  ({
     color,
     message,
 });
+
 const observer = useMemo(()=> new IntersectionObserver(([entry])))
 const data_fectch = await fetch('https://');
 const toggleStatus = (id)
+
 const Avatar=()=>{
     const dataAnalytics = {pageId:"demo"};
     userEffect(()=>{
@@ -40,4 +47,44 @@ const Avatar=()=>{
     }, []);
     return <div className = 'tutorial-short'></div>; 
 }
+//
+export class CartService
+{
+  protected cartList: Cart[] = []
+  constructor(private prod: ProductService) { }
+  
+  getCartAll() {
+    
+    return this.cartList
+  }
+  getInStock(id: number) { 
+    
+    return this.cartList.find( i=> i.Id == id)?.InStock
+    
+  }
+  addCart(index: number, frmProduct: any){
+    let itemCart = this.cartList.filter(i => i.Id == this.prod);
+  }
+}
+
+const router = [
+    { path: '', component: HomeComponent, title: "Home"},
+    {
+      path: 'product-detail/:id',
+      component : ProductDetailComponent,
+      title: 'Product Detail',
+    },
+];
+@NgModule( 
+    {
+      declaration: [
+        AppComponent,
+        HomeComponent,
+        ProductListComponent,
+        CartService,
+      ]
+    }
+)
+
+
 export default Avatar;
