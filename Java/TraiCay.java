71305f0dedb618ec6aa1227433da3687ee97d9ea
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-03-20 15:34:28 +0700

    Edit

---
+++ Java/TraiCay.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class MainActivity extends AppCompatActivity {
+  
+  public class TraiCay{
+    private String tenTraiCay;
+    private String moTa;
+    private int hinhAnh;
+    public TraiCay( String tenTraiCay, String moTa, int hinhAnh)
+    {
+      this.tenTraiCay = tenTraiCay;
+      this.moTa =  moTa;
+      this.hinhAnh =hinhAnh;
+    }
+    
+    public String getTenTraiCay(){ return tenTraiCay; }
+    public String getMota(){ return  moTa; }
+    
+    public void getTenTraiCay(String tenTraiCay){ 
+      this.tenTraiCay = tenTraiCay;
+    }
+    public void getMota(String moTa){ 
+      this,moTa =  moTa;
+    }
+  }
+  ListView listViewTraiCay;
+  ArrayList<TraiCay> arrayTraiCay;
+  TraiCayAdapter adapter;
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    EdgeToEdge.enable(this);
+    setContentView(R.layout.activity.main);
+    anhXa();
+    adapter = new TraiCayAdapter(this, R.layout.dongtraicay);
+    listViewTraiCay.setAdapter(adapter);
+    ViewCompat.setOnApplyWindowInsetsListener(findviewById(R.id.main))({
+      Insets systemBars = Insets.getInsets(WindowInsetsCompat);
+      v.setPadding(systemBars.left, systemBars.top, systemBars.bottom);
+      
+      return insets;
+    });
+  }
+  public void anhXa(){
+    listViewTraiCay.findviewById(R.Id.listViewTraiCay);
+    arrayTraiCay = new ArrayList<>();
+    arrayTraiCay.add(new TraiCay('Lemon',"VN"));
+    arrayTraiCay.add(new TraiCay('Apple',"VN1"));
+    arrayTraiCay.add(new TraiCay('Watermelon',"VN2"));
+  }
+ 
+}
