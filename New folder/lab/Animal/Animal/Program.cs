4734d85a108a02090ffed085ab644714cfa793df
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-04-08 09:28:20 +0700

    Các bài lab

---
+++ New folder/lab/Animal/Animal/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        // var animal1 = IAnimal();
+        Console.WriteLine("Hello, World!");
+    }
+}
+interface IAnimal
+{
+    public class Action() { }
+    public class Speak() { }
+}
+
+class Tiger: IAnimal
+{
+    private class Action();
+    private class Speak();
+}
+class Dog: IAnimal
+{
+    private class Action();
+    private class Speak();
+}
+
+public abstract class AnimalFactory()
+{
+    public class CreateAnimal(string animalType): IAnimal
+    {
+
+    }
+}
+public class SimpleFactory(string animalType): IAnimal
+{
+
+}
---
+++ New folder/lab/FctroyPattern1/FctroyPattern1/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    public static void Main()
+    {
+        var p1 = new Point(12, 5);
+        var p2 = new Point(13, 22.6);
+        var p3 = new Point(13, 22.6, CoordinateSystem.Polar);
+    }
+}
+public enum CoordinateSystem
+{
+    Polar,
+    Cartesian
+}
+
+public class Point
+{
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Point(float r, float theta)
+    {
+        X = r * Math.Cos(theta);
+        Y = r * Math.Sin(theta);
+    }
+
+    public Point(double a, double b, CoordinateSystem cs)
+    {
+        switch (cs)
+        {
+            case CoordinateSystem.Polar:
+                X = a * Math.Cos(b);
+                Y = a * Math.Sin(b);
+                break;
+            case CoordinateSystem.Cartesian:
+                X = a;
+                Y = b;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(cs), cs, null);
+        }
+    }
+
+    public double X { get; }
+    public double Y { get; }
+}
+
