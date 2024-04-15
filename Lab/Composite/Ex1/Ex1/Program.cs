cfee0022e365d1debdebd14f18d1905e2b8456c1
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-04-15 10:55:28 +0700

    Add files via upload

---
+++ Lab/Composite/Ex1/Ex1/Program.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.IO;
+using System.Data.Common;
+using System.Text.RegularExpressions;
+using System.IO.Compression;
+using System.Runtime.CompilerServices;
+using System.Xml.Schema;
+
+namespace Ex1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public interface Graphic
+    {
+        void move(int x, int y);
+        void draw();
+    }
+    public abstract class Dot : Graphic
+    {
+        protected int x { get; set; }
+        protected int y { get; set; }
+        public Dot(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public class Move(int x, int y) { 
+            
+        }
+        public void draw() { }
+
+        public abstract void move(int x, int y);
+    }
+    public class CompoundGraphic 
+    {
+        private readonly Graphic[] children;
+        public void add(Graphic child) { }
+        public void remove(Graphic child) { }
+        public class Move(int x, int y)
+        {
+
+        }
+        public void Draw() { }
+
+    }
+    public abstract class Circle
+    {
+        public double radius { get; set; }
+
+        public Circle(int x, int y, double radius) { }
+
+    
+    }
+}
---
+++ Lab/Composite/Ex2/Ex2/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Text;
+using static Ex2.GraphObject;
+
+namespace Ex2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var drawing = new GraphObject { Name = "My Drawing" };
+            var group = new GraphObject();
+            drawing.Children.Add(new Square { Color = "Red" });
+            group.Children.Add(new Square { Color = "Blue" });
+            Console.WriteLine(drawing);
+
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+
+    public class GraphObject
+    {
+        public string Name = "Group";
+        public string Color { get; set; }
+        public List<GraphObject> Children;
+
+        private class Print(StringBuilder sb, int depth)
+        {
+            
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            // Bug
+            Print(sb, 0);
+
+            return sb.ToString();
+        }
+
+        public class Square: GraphObject
+        {
+            public new string Name => nameof(Square);
+        }
+        public class Circle: GraphObject
+        {
+            public new string Name => nameof(Circle);
+        }
+    }
+}
