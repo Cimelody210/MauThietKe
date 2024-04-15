301881ae2f779c9573b2257f515e271f1ee87167
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-04-15 07:41:51 +0700

    Add files via upload

---
+++ Lab/Adapter/Ex2/Ex2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+//Integrate two exchange rate services, VCB and DAB, into an existing program with a hard-coded exchange rate interface
+// VCB and DAB have different methods for fetching rates.
+
+namespace Ex2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var service = new ExchangeRateService();
+            //Client(service);
+
+        }
+    }
+    public class ExchangeRate
+    {
+        public required string CurrencyCode { get; set; }
+        public double Buy { get; set; }
+        public double Sell { get; set; }
+    }
+
+    public interface IExchangeRateService
+    {
+        List<ExchangeRate> GetList();
+    }
+
+    public class ExchangeRateService : IExchangeRateService
+    {
+        public List<ExchangeRate> GetList()
+        {
+            return new List<ExchangeRate>
+            {
+                new() { CurrencyCode = "USD", Buy = 24_000, Sell = 24_500 }
+            };
+        }
+    }
+}
---
+++ Lab/Adapter/Exercises1/Exercises1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Data.Common;
+
+namespace Exercises1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var hole = new RoundHole(5);
+            var rpeg = new RoundPeg(5);
+            Console.WriteLine(rpeg);
+            Console.WriteLine(hole);
+            Console.WriteLine("Example of Adapter in square and circle");
+
+        }
+    }
+    public class RoundHole
+    {
+        public int Radius { get; set; }
+        public RoundHole(int radius)
+        {
+            Radius = radius;
+
+        }
+        public bool Fits(RoundHole peg)
+        {
+            return true;
+        }
+    }
+    public class RoundPeg
+    {
+        public int Radius { get; set; }
+        public RoundPeg(int radius)
+        {
+            Radius = radius;
+        }
+    }
+    public class SquarePegAdapter: RoundPeg
+    {
+        private SquarePeg peg;
+        public int getRadius
+        {
+            get { return (int)(peg.Width * Math.Sqrt(2) / 2); }
+        }
+    
+
+        public SquarePegAdapter(SquarePeg peg): base(peg.Width)
+        {
+            this.peg = peg;
+        }
+
+    }
+    public class SquarePeg
+    {
+        public int Width { get; set; }
+
+    
+        public SquarePeg(int width)
+        {
+            width = Width;
+        }
+
+        public double CaculateArea()
+        {
+            return Width * Width;
+        }
+    }
+}
+
