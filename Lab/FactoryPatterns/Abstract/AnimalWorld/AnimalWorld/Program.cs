41e0f740f535eb3a52247589a0208082f0340690
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-04-22 09:22:37 +0700

    Add files via upload

---
+++ Lab/FactoryPatterns/Abstract/AnimalWorld/AnimalWorld/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+namespace AnimalWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IContinentFactory continent = new AmericaFactory();
+            AnimalWorld world = new AnimalWorld(continent);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public abstract class Hebrivore
+    {
+
+    }
+    public class Bison : Hebrivore { }
+    public class Wildebesst : Hebrivore { }
+    public class Deer : Hebrivore { }
+    public abstract class Carnivore
+    {
+        public class Eats(Hebrivore hebrivore)
+        {
+
+        }
+    }
+    public class Wolf: Carnivore { }
+    public class Lion: Carnivore { }
+    public class Tiger: Carnivore { }
+    
+    interface IContinentFactory
+    {
+        public Hebrivore CreateHebrivore {  get;  }
+        public Carnivore CreateCarnivore {  get;  }
+
+    }
+    public class Africafactory : IContinentFactory
+    {
+        public Hebrivore CreateHebrivore => throw new NotImplementedException();
+
+        public Carnivore CreateCarnivore => throw new NotImplementedException();
+    }
+    public class AsiaFactory : IContinentFactory
+    {
+        public Hebrivore CreateHebrivore => throw new NotImplementedException();
+
+        public Carnivore CreateCarnivore => throw new NotImplementedException();
+    }
+    public class AmericaFactory : IContinentFactory
+    {
+        public Hebrivore CreateHebrivore => throw new NotImplementedException();
+
+        public Carnivore CreateCarnivore => throw new NotImplementedException();
+    }
+    public class AnimalWorld
+    {
+        private Carnivore _carnivore;
+        private Hebrivore _hebrivore;
+        private IContinentFactory continent;
+
+        public void RunFoodChain()
+        {
+            //bug
+            //_carnivore.Eats(_hebrivore);
+        }
+
+        // Bug
+        public AnimalWorld(IContinentFactory continent)
+        {
+            this.continent = continent;
+        }
+    }
+
+}
---
+++ Lab/FactoryPatterns/Abstract/Ex2_Animal/Ex2_Animal/Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Create the implementation for the following class diagram using Abstract Factory pattern
+using System;
+using System.Text;
+using System.Collections.Generic;
+using Ex2_Animal;
+
+namespace Ex2_Animal
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***Abstract Factory Pattern Demo***\n");
+            var wildAnimalFactory = new WildAnimalFactory();
+            // bug
+            //var wildDog = wildAnimalFactory.GetDog();
+           
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+    public class WildAnimalFactory
+    {
+        // bí ý tưởng mọe roi :)
+    }public class PetAnimalFactory
+    {
+        // bí ý tưởng mọe roi :)
+    }
+    interface IDog
+    {
+        public class Action();
+        public class Speak();
+    }
+    public class WildDog: IDog
+    {
+        public class Action();
+        public class Speak();
+    }
+    public class PetDog: IDog
+    {
+        public class Action();
+        public class Speak();
+    }
+    interface ITiger
+    {
+        public class Action();
+        public class Speak();
+    }
+    public class WildTiger: ITiger
+    {
+        public class Action();
+        public class Speak();
+    }
+    public class PetTiger: ITiger
+    {
+        public class Action();
+        public class Speak();
+    }
+    interface IAnimalFactory
+    {
+        public class GetDog: IDog;
+        public class GetTiger: ITiger;
+    }
+    public class WildFactory: IAnimalFactory
+    {
+        public class GetDog: IDog;
+        public class GetTiger: ITiger;
+    }
+    public class PetFactory: IAnimalFactory
+    {
+        public class GetDog: IDog;
+        public class GetTiger: ITiger;
+    }
+
+    
+    
+
+}
+    
+ 
