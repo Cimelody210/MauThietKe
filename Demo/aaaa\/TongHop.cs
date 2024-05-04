f795093992331706ed2287e22ba6f2451bff74ac
Nguyễn Tín <144228743+Cimelody210@users.noreply.github.com>
2024-05-04 14:51:37 +0700

    dfg

---
+++ Demo/aaaa\/TongHop.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Collection.Generic;
+namespace TongHop
+{
+    public class TongHop
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+    //test thử nghiệm
+    private static final EngeSingleton instance  = new EngeSingleton();
+    public class A{
+        var user = GetUser()
+        if(user is not null && user.Name.Length > 0 && user.Subscription is not null){
+            Console.WriteLine("Success");
+        }
+        if(user is Name.Length: >0){
+            Console.WriteLine("Success");
+        }
+    }
+}
